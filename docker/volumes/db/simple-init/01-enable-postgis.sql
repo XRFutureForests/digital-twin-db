9aac78a9c42006d47b78839c79d22a3a6d536e4e
Maximilian Sperlich <maximilian.sperlich@gmail.com>
2026-01-15 12:12:28 +0100

    Refactor SQL scripts to enhance PostGIS extension setup and improve species mapping logic in CSV import

--- docker/volumes/db/simple-init/01-enable-postgis.sql
+++ docker/volumes/db/simple-init/01-enable-postgis.sql
@@ -11,8 +11,12 @@ CREATE SCHEMA IF NOT EXISTS extensions;
 -- Enable PostGIS extension in public schema (simpler, avoids search path issues)
 CREATE EXTENSION IF NOT EXISTS postgis CASCADE;
 
--- Add extensions to search path for all sessions
-ALTER DATABASE forest_twin SET search_path TO "$user", public, extensions;
+-- Add extensions to search path for all sessions (use current database name)
+DO $$
+BEGIN
+    EXECUTE format('ALTER DATABASE %I SET search_path TO "$user", public, extensions', current_database());
+END
+$$;
 
 -- Also set for current session
 SET search_path TO "$user", public, extensions;
